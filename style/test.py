3a81e6ddd893844097d0db7481dfae716ed41028
Anahis259 <71285436+Anahis259@users.noreply.github.com>
2022-07-05 16:31:23 -0300

    inicio de mi webSite

---
+++ style/test.py
@@ -0,0 +1,20 @@
+"""lista = [1,3,4]
+for i in range(len(lista)):
+    lista.insert(1,lista[i])
+print(lista)   
+
+i = 1
+while i < 10:
+    print("#")
+    i = i << 1
+list= [3,1,-2]    
+print(list[list[-1]])
+
+x = 1
+x= x==x 
+print(x)
+n = [1,3,4]
+vals = n[-1:-2]
+print(n)
+print(vals)"""
+
